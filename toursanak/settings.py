6f2d4ba8146274834380abf4c7d6776d3ba9b8f3
user0 <kimsalsan007@gmail.com>
2016-05-28 10:17:33 +0700

    clean coding23

--- toursanak/settings.py
+++ toursanak/settings.py
@@ -55,12 +55,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'toursanak',
-        'USER': 'toursanak',
-        'PASSWORD': 'toursanak',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
 
 
 #kimsal
