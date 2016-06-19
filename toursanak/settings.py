100109bc1c275fa6ca0fb68d3dbedc73d919b4f9
user0 <kimsalsan007@gmail.com>
2016-06-19 11:26:35 +0700

    correct style

--- toursanak/settings.py
+++ toursanak/settings.py
@@ -52,12 +52,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'toursanak',
-        'USER': 'toursanak',
-        'PASSWORD': 'toursanak',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql', 
